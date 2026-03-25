8f5b4b22135a55dbc94e0b0e3d796d64b0cc09f7
WesllenCarmo <wesllenaraujo.developer@gmail.com>
2026-03-25 13:48:18 -0700

    Create Skills component's structure.

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import WesllenIcon from '../../../assets/wesllen-cpu-icon.webp';
 import Resume from '../../../../Documents/wesllen_araujo_resume.pdf';
 import About from '../About';
+import Skills from '../Skills';
 
 const Home = () => {
   return (
@@ -28,6 +29,7 @@ const Home = () => {
       </div>
       <div className='flex flex-col gap-16'>
         <About />
+        <Skills />
       </div>
     </div>
   );
--- src/components/pages/Skills/index.jsx
+++ src/components/pages/Skills/index.jsx
@@ -2,7 +2,35 @@ import React from 'react'
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <section id='skills'>
+      <div>
+        <h4>Soft skills</h4>
+        <div>
+          <p>Organisation</p>
+          <p>Logical thinking</p>
+          <p>Communication</p>
+        </div>
+      </div>
+      <div>
+        <h4>hard skills</h4>
+        <div>
+          <p>HTML5</p>
+          <p>CSS3</p>
+          <p>JavaScript</p>
+          <p>React.js</p>
+          <p>Responsive Web Design</p>
+          <p>Tailwind CSS</p>
+        </div>
+      </div>
+      <div>
+        <h4>Tools & Version Control</h4>
+        <div>
+          <p>Figma</p>
+          <p>Git</p>
+          <p>GitHub</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
