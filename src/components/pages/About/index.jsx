99c43b54d7d67860e87fe1b152790540e6323c46
WesllenCarmo <wesllenaraujo.developer@gmail.com>
2026-03-25 08:11:45 -0700

    Create About Me component's structure.

--- src/components/pages/About/index.jsx
+++ src/components/pages/About/index.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h2>About Me</h2>
+      <div>
+        <p>I'm a Front End Web Developer and also a Computer Network Technician who is passionate about <span>understanding & bulding system</span>.</p>
+        <p>Besides, I'm an exchange student of PGM (Programa Ganhe o Mundo), which is responsible for taking me to Vancouver (Canada) in order to improve my <span>English skills</span>.</p>
+      </div>
+    </section>
   )
 }
 
--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import WesllenIcon from '../../../assets/wesllen-cpu-icon.webp';
 import Resume from '../../../../Documents/wesllen_araujo_resume.pdf';
+import About from '../About';
 
 const Home = () => {
   return (
-    <div className="flex font-bold w-full h-screen justify-between px-52 items-center">
-      <div className='flex flex-col gap-4'>
-        <h3 className='text-3xl text-custom-blue'>Hello, World!</h3>
-        <h2 className='text-5xl text-custom-white'>I'm Wesllen do Carmo</h2>
-        <h1 className='text-8xl text-custom-light-blue'>Front End <br /> Web Developer</h1>
-        <div className='flex justify-between flex-wrap'>
-          <a href='/#contact' className='bg-custom-light-blue border-4 border-custom-light-blue text-custom-dark-blue text-3xl font-bold px-10 py-2 rounded-xl transition duration-400 hover:bg-custom-dark-blue hover:text-custom-light-blue'>
-            Contact me
-          </a>
-          <a href={Resume} download className='border-custom-light-blue border-4 text-3xl text-custom-light-blue font-bold px-6 py-1 rounded-xl hover:bg-custom-light-blue hover:text-custom-dark-blue transition duration-400'>
-            Download résumé
-          </a>
+    <div>
+      <div className="flex font-bold w-full h-screen justify-between px-52 items-center">
+        <div className='flex flex-col gap-4'>
+          <h3 className='text-3xl text-custom-blue'>Hello, World!</h3>
+          <h2 className='text-5xl text-custom-white'>I'm Wesllen do Carmo</h2>
+          <h1 className='text-8xl text-custom-light-blue'>Front End <br /> Web Developer</h1>
+          <div className='flex justify-between flex-wrap'>
+            <a href='/#contact' className='bg-custom-light-blue border-4 border-custom-light-blue text-custom-dark-blue text-3xl font-bold px-10 py-2 rounded-xl transition duration-400 hover:bg-custom-dark-blue hover:text-custom-light-blue'>
+              Contact me
+            </a>
+            <a href={Resume} download className='border-custom-light-blue border-4 text-3xl text-custom-light-blue font-bold px-6 py-1 rounded-xl hover:bg-custom-light-blue hover:text-custom-dark-blue transition duration-400'>
+              Download résumé
+            </a>
+          </div>
         </div>
+        <img
+          src={WesllenIcon}
+          alt="Wesllen image: A brown man with black glasses, dark brown eyes and coily hair."
+          className='w-1/3'
+        />
       </div>
-      <img
-        src={WesllenIcon}
-        alt="Wesllen image: A brown man with black glasses, dark brown eyes and coily hair."
-        className='w-1/3'
-      />
+      <About />
     </div>
   );
 };
