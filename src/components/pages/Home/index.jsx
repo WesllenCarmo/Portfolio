6ecd574a4aeccc9d22d095985f83ef9100c68ce3
WesllenCarmo <wesllenaraujo.developer@gmail.com>
2026-03-24 21:27:33 -0700

    Create structure of Hero section.

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import WesllenIcon from '../../../assets/wesllen-cpu-icon.webp'
+import Resume from '../../../../Documents/wesllen_araujo_resume.pdf'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <img
+      src={WesllenIcon}
+      alt='Wesllen image: A brown man with black glasses, dark brown eyes and coily hair.'
+      />
+      <div>
+        <h3>Hello, World!</h3>
+        <h2>I'm Wesllen do Carmo</h2>
+        <h1>Front End Web Developer</h1>
+        <div>
+          <button>Contact me</button>
+          <a href={Resume} download>Download résumé</a>
+        </div>
+      </div>
+    </div>
   )
 }
 
