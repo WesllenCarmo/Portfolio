f0808286a54093c27eebc23fec4c4dd8b10ed6a4
WesllenCarmo <wesllenaraujo.developer@gmail.com>
2026-03-24 21:16:13 -0700

    Add styles of React Router navigation.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,20 +5,26 @@ import WLogo from '/w-logo.webp'
 
 const NavBar = () => {
   return (
-    <nav>
+    <nav className='flex justify-between w-full bg-custom-dark-blue text-custom-white items-center py-4 px-16'>
       <div>
         <Link to='/'>
-          <img src={WLogo} alt="To arrow symbols with a 'W' letter in between." />
+          <img
+          src={WLogo}
+          alt="To arrow symbols with a 'W' letter in between."
+          className='w-30'
+          />
         </Link>
       </div>
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/#about'>About me</Link>
-        <Link to='/#skills'>Skills</Link>
-        <Link to='/#projects'>Projects</Link>
-        <Link to='/#contact'>Contact</Link>
+      <div className='flex gap-16 text-2xl'>
+        <Link to='/' className='hover:text-custom-blue transition duration-300'>Home</Link>
+        <Link to='/#about' className='hover:text-custom-blue transition duration-300'>About me</Link>
+        <Link to='/#skills' className='hover:text-custom-blue transition duration-300'>Skills</Link>
+        <Link to='/#projects' className='hover:text-custom-blue transition duration-300'>Projects</Link>
+        <Link to='/#contact' className='hover:text-custom-blue transition duration-300'>Contact</Link>
       </div>
+      
     </nav>
+    
   )
 }
 
