d29fe2be3fcc77969e4f4a5c72a27e34c0361442
WesllenCarmo <wesllenaraujo.developer@gmail.com>
2026-03-26 12:13:23 -0700

    Change the arrow from the ToTop button and its size.

--- src/components/pages/BackToTopButton.jsx
+++ src/components/pages/BackToTopButton.jsx
@@ -1,6 +1,6 @@
 import Raect from 'react'
 import { useEffect, useState } from 'react'
-import { GoArrowUp } from 'react-icons/go'
+import { SlArrowUp } from 'react-icons/sl'
 
 const BackToTopButton = () => {
   const [ backToTopButton, setBackToTopButton ] = useState(false)
@@ -24,7 +24,7 @@ const BackToTopButton = () => {
   return (
     <div>
       {backToTopButton && (
-        <button onClick={scrollUp} className='fixed bottom-20 right-20 flex justify-center items-center w-20 h-20 bg-custom-light-blue text-5xl font-bold rounded-full text-custom-dark-blue hover:bg-custom-blue transition duration-400 cursor-pointer'><GoArrowUp /></button>
+        <button onClick={scrollUp} className='fixed bottom-20 right-20 flex justify-center items-center w-20 h-20 bg-custom-light-blue text-4xl font-bold rounded-full text-custom-dark-blue hover:bg-custom-blue transition duration-400 cursor-pointer'><SlArrowUp /></button>
       )}
     </div>
   )
