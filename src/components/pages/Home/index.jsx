5cc77b59f32f21ab7a753da956ff69e85e96f5cb
WesllenCarmo <wesllenaraujo.developer@gmail.com>
2026-03-25 22:30:03 -0700

    Create footer with social media links. Add id 'home' to hero section div.

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import WLogo from '/w-logo.webp'
 import WesllenIcon from '../../../assets/wesllen-cpu-icon.webp';
 import Resume from '../../../../Documents/wesllen_araujo_resume.pdf';
 import About from '../About';
 import Skills from '../Skills';
 import Projects from '../Projects';
 import Contact from '../Contact';
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+import { SiGmail } from 'react-icons/si'
 
 const Home = () => {
   return (
     <div>
-      <div className="flex font-bold w-full h-screen justify-between px-52 items-center">
+      <section id='home' className="flex font-bold w-full h-screen justify-between px-52 items-center">
         <div className='flex flex-col gap-4'>
           <h3 className='text-3xl text-custom-blue'>Hello, World!</h3>
           <h2 className='text-5xl text-custom-white'>I'm Wesllen do Carmo</h2>
@@ -28,13 +31,27 @@ const Home = () => {
           alt="Wesllen image: A brown man with black glasses, dark brown eyes and coily hair."
           className='w-1/3'
         />
-      </div>
-      <div className='flex flex-col gap-16'>
+      </section>
+      <div className='flex flex-col gap-16 mb-8'>
         <About />
         <Skills />
         <Projects />
         <Contact />
       </div>
+      <footer className='border-t-2 px-8 py-4 border-custom-light-blue flex justify-between'>
+        <a href='#home'>
+          <img
+            src={WLogo}
+            alt="To arrow symbols with a 'W' letter in between."
+            className='w-30'
+          />
+        </a>
+        <div className='flex gap-8'>
+          <a href='https://github.com/WesllenCarmo' target='_blank'><FaGithub className='text-5xl text-custom-white' /></a>
+          <a href='https://www.linkedin.com/in/wesllen-araujo0907/' target='_blank'><FaLinkedin className='text-5xl text-custom-white' /></a>
+          <a href='mailto:wesllenaraujo.developer@gmail.com' target='_blank'><SiGmail className='text-5xl text-custom-white' /></a>
+        </div>
+      </footer>
     </div>
   );
 };
