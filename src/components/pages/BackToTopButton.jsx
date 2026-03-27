4c330abd2240d5fa7bc117fd9774fe090da5761f
WesllenCarmo <wesllenaraujo.developer@gmail.com>
2026-03-26 20:24:16 -0700

    Adjust ToTop button's size in small screens.

--- src/components/pages/BackToTopButton.jsx
+++ src/components/pages/BackToTopButton.jsx
@@ -24,7 +24,7 @@ const BackToTopButton = () => {
   return (
     <div>
       {backToTopButton && (
-        <button onClick={scrollUp} className='fixed bottom-20 right-20 flex justify-center items-center w-20 h-20 bg-custom-light-blue text-4xl font-bold rounded-full text-custom-dark-blue hover:bg-custom-blue transition duration-400 cursor-pointer'><SlArrowUp /></button>
+        <button onClick={scrollUp} className='fixed bottom-10 lg:bottom-20 right-10 lg:right-20 flex justify-center items-center w-15 lg:w-20 h-15 lg:h-20 bg-custom-light-blue text-3xl font-bold rounded-full text-custom-dark-blue hover:bg-custom-blue transition duration-400 cursor-pointer'><SlArrowUp /></button>
       )}
     </div>
   )
