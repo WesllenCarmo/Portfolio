25d0516bac1b5ec93493f22c58c80dc49e9722b2
WesllenCarmo <wesllenaraujo.developer@gmail.com>
2026-03-26 20:11:27 -0700

    Add responsiveness to Contact component's styles.

--- src/components/pages/Contact/index.jsx
+++ src/components/pages/Contact/index.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <section id='contact' className='flex flex-col gap-16 text-custom-white'>
+    <section id='contact' className='flex flex-col gap-16 text-custom-white px-3 lg:px-0'>
       <h2 className='text-custom-light-blue font-bold text-5xl self-center'>Contact</h2>
-      <form method='POST' action="https://formsubmit.co/wesllenaraujo.developer@gmail.com" className='bg-custom-blue-modified font-bold text-2xl w-2/3 self-center flex flex-col px-8 py-8 gap-8 rounded-2xl'>
+      <form method='POST' action="https://formsubmit.co/wesllenaraujo.developer@gmail.com" className='bg-custom-blue-modified font-bold text-2xl w-full lg:w-2/3 self-center flex flex-col px-8 py-8 gap-8 rounded-2xl'>
         <h3 className='text-4xl font-bold'>Get in touch</h3>
-        <div className='flex w-full justify-between gap-8'>
-          <div className='flex flex-col w-1/2 gap-2'>
+        <div className='flex flex-col lg:flex-row w-full justify-between gap-8'>
+          <div className='flex flex-col w-full lg:w-1/2 gap-2'>
             <label htmlFor="firstName">First name:</label>
             <input type="text" name="firstName" id="firstName" required className='font-normal bg-custom-white text-custom-dark-blue rounded-md px-2 py-2'/>
             <span className='text-xl opacity-50'>Enter your first name.</span>
           </div>
-          <div className='flex flex-col w-1/2 gap-2'>
+          <div className='flex flex-col w-full lg:w-1/2 gap-2'>
             <label htmlFor="lastName">Last name:</label>
             <input type="text" name="lastName" id="lastName" required className='font-normal bg-custom-white text-custom-dark-blue rounded-md px-2 py-2'/>
             <span className='text-xl opacity-50'>Enter your last name.</span>
@@ -28,7 +28,7 @@ const Contact = () => {
           <textarea name="message" id="message" rows='5' required  className='font-normal bg-custom-white text-custom-dark-blue rounded-md px-2 py-2 resize-none'></textarea>
           <span className='text-xl opacity-50'>Enter your message.</span>
         </div>
-        <button type="submit" className='text-custom-light-blue border-4 border-custom-light-blue w-fit self-center px-32 py-4 text-3xl rounded-xl hover:bg-custom-light-blue hover:text-custom-blue-modified transition duration-400 cursor-pointer'>Send message</button>
+        <button type="submit" className='text-custom-light-blue border-4 border-custom-light-blue w-fit self-center px-12  lg:px-32 py-4 text-3xl rounded-xl hover:bg-custom-light-blue hover:text-custom-blue-modified transition duration-400 cursor-pointer'>Send message</button>
       </form>
     </section>
   )
